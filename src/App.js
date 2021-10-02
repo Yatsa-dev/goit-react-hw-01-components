3edc89d7f5357f83877cc66c696e720d31af8815
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-10-02 21:06:56 +0300

    added profile component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 // import "./App.css";
+import Profile from './components/Profile';
+import user from './API/user.json';
+function App() {
+  return (
+    <div>
+      <Profile
+        image={user.avatar}
+        title={user.name}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+}
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
