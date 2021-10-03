fc861d2233bffb871053142ad6bab388b770b603
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-10-03 18:35:22 +0300

    Updates

--- static/js/main.67e4b1a1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,t){}],[[0,1]]]);
-//# sourceMappingURL=main.67e4b1a1.chunk.js.map
